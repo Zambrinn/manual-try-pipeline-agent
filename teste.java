682ae45abd44a5029176abe39e8d336ef8d97554
Zambrinn <zambrinfilho@gmail.com>
2026-04-06 11:24:11 -0300

    feat: add initial implementation of teste class with main method

---
+++ teste.java
@@ -0,0 +1,8 @@
+public class teste {
+    public static void main(String[] args) {
+        double numHash = 0.2;
+        byte num = 1;
+        System.err.println("teste oi" + numHash + num);
+    }
+
+}
